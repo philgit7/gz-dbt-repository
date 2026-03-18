72a889c684984625b4acbfba6f6faed3347568de
Philcost <philibert.gouveia@gmail.com>
2026-03-18 15:38:35 +0000

    margin + purchase_cost

--- models/int_sales_margin.sql/int_sales_margin.sql
+++ models/int_sales_margin.sql/int_sales_margin.sql
@@ -7,8 +7,26 @@ WITH purchase_cost AS (
     INNER JOIN {{ ref('stg_raw__product') }} AS p
     ON s.products_id = p.products_id
     GROUP BY date_date, orders_id 
+),
+
+margin AS (
+    SELECT 
+      date_date
+    , orders_id
+    , ROUND(SUM(s.revenue) - SUM(p.purchase_price), 0)
+    FROM {{ ref('stg_raw__sales') }} AS s
+    INNER JOIN {{ ref('stg_raw__product') }} AS p
+    ON s.products_id = p.products_id
+    GROUP BY date_date, orders_id 
 )
 
 SELECT 
-*
-FROM purchase_cost;
+  pc.date_date
+, pc.orders_id
+, pc.purchase_cost
+, m.margin 
+FROM purchase_cost AS pc 
+LEFT JOIN margin AS m
+ON  pc.date_date = m.date_date
+AND pc.orders_id = m.orders_id
+
