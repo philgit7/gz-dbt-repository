bf8853ccfd467e71e50a6a8ff6457d7ea3f4a6a6
Philcost <philibert.gouveia@gmail.com>
2026-03-19 10:13:36 +0000

    change

--- models/int_orders_margin.sql/int_orders_operational.sql
+++ models/int_orders_margin.sql/int_orders_operational.sql
@@ -6,6 +6,6 @@ SELECT
 ,   ROUND(SUM(quantity), 2) AS quantity
 ,   ROUND(SUM(purchase_cost), 2) AS purchase_cost
 ,   ROUND(SUM(margin), 2) AS margin
-FROM {{ ref("int_sales_margin") }} AS s
+FROM {{ ref("int_orders_marginn") }} AS s
 GROUP BY orders_id
 ORDER BY orders_id DESC
